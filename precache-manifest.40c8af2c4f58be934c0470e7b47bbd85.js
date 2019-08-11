0a3276e7476d4971c5222681c21e95df7924f283
Dimitri Subrini <dimitri.subrini@gmail.com>
2019-08-12 01:49:42 +0200

    Updates

---
+++ precache-manifest.40c8af2c4f58be934c0470e7b47bbd85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9435a019d35cedc47333f6d1c96076ea",
+    "url": "/robofriends_PWA/index.html"
+  },
+  {
+    "revision": "e928ae7a21daf71725d6",
+    "url": "/robofriends_PWA/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "548e284c93195520dda1",
+    "url": "/robofriends_PWA/static/css/main.c2cc023f.chunk.css"
+  },
+  {
+    "revision": "e928ae7a21daf71725d6",
+    "url": "/robofriends_PWA/static/js/2.71409af7.chunk.js"
+  },
+  {
+    "revision": "548e284c93195520dda1",
+    "url": "/robofriends_PWA/static/js/main.29f839e4.chunk.js"
+  },
+  {
+    "revision": "6ad4b6cf07c5da0f8db2",
+    "url": "/robofriends_PWA/static/js/runtime~main.f31f2021.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends_PWA/static/media/SEGA.12a3db8b.woff"
+  }
+]);
